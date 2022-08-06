874bf38cdeeed368df5a9eea78ea30f961160664
Takeme-Steph <stephanie.takeme@gmail.com>
2022-08-06 23:30:28 +0100

    refactored the code, switched to an ORM, added an extra variable

--- finance/app.py
+++ finance/app.py
@@ -1,11 +1,15 @@
+import email
 import os
 import json
-from cs50 import SQL
+from turtle import update
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+import sqlalchemy.types as types
 
 from helpers import apology, login_required, lookup, usd
 
@@ -34,32 +38,63 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+# Configure SQL alchemy database
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///finance.db"
+db = SQLAlchemy(app)
 
 # Make sure API keys are set
 if not os.environ.get("API_KEY"):
     raise RuntimeError("IEX API_KEY not set")
 if not os.environ.get("RAVE_KEY"):
     raise RuntimeError("RAVE API_KEY not set")
+if not os.environ.get("PAYMENT_CALLBACK"):
+    raise RuntimeError("Payment gateway callback url not set")
 
+
+#create models
+class History(db.Model):
+    PK = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer)
+    name = db.Column(db.String(50))
+    symbol = db.Column(db.String(50))
+    price = db.Column(db.Float)
+    amount = db.Column(db.Integer)
+    cost = db.Column(db.Float)
+    type = db.Column(db.String(50))
+    date = db.Column(db.DateTime, default=datetime.now)
+    balance = db.Column(db.Integer)
+
+class Portfolio(db.Model):
+    PK = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer)
+    stock = db.Column(db.String(50))
+    symbol = db.Column(db.String(50))
+    amount = db.Column(db.Integer)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    hash = db.Column(db.String(200))
+    cash = db.Column(db.Float)
+    email = db.Column(db.String(100))
+    date = db.Column(db.DateTime, default=datetime.now)
 
 @app.route("/", methods=["GET"])
 @login_required
 def index():
     """Show portfolio of stocks"""
-    portfolio = db.execute("SELECT * FROM portfolio WHERE userid=:ids", ids=session["user_id"])
+    portfolio = Portfolio.query.filter_by(userid=session["user_id"]).all()
 
     # get current price of stocks by passing each symbol to the lookup fucntion
-    j = len(portfolio)
-    for i in range(j):
-        p = lookup(portfolio[i]["symbol"])
-        portfolio[i]["price"] = p["price"]
+    for i in portfolio:
+        p = lookup(i.symbol)
+        i.price = p["price"]
+    
 
     # get user details
-    user = db.execute("SELECT * FROM users WHERE id=:ids", ids=session["user_id"])
+    user = Users.query.filter_by(id=session["user_id"]).first()
 
-    return render_template("index.html", portfolio=portfolio, j=j, usd=usd, user=user)
+    return render_template("index.html", portfolio=portfolio, usd=usd, user=user)
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -88,27 +123,34 @@ def buy():
 
         cost = shares * (results["price"])
         # get user data
-        rows = db.execute("SELECT * FROM users WHERE id = :ids",
-                          ids=session["user_id"])
+        rows = Users.query.filter_by(id=session["user_id"]).first()
         # check user has enough funds
-        money = rows[0]["cash"]
+        money = rows.cash
         if money < cost:
             return apology("insufficient funds")
         # update db
         else:
+            update_user = Users.query.get(session["user_id"])
             balance = (money - cost)
-            db.execute("UPDATE users SET cash=:balance WHERE id=:ids", balance=balance, ids=session["user_id"])
-            db.execute("INSERT INTO history (userid, name, symbol, price, cost, amount, type, balance) VALUES (:ids, :name, :symbol, :price, :cost, :amount, :ttype, :balance)",
-                       ids=session["user_id"], name=results["name"], symbol=results["symbol"], price=results["price"], cost=cost, amount=shares, ttype="buy", balance=balance)
+
+            update_user.cash = balance
+            db.session.commit()
+
+            history_new = History(userid=session["user_id"], name=results["name"], symbol=results["symbol"], 
+            price=results["price"], cost=cost, amount=shares, type="buy", balance=balance)
+            db.session.add(history_new)
+            db.session.commit()
 
         # update portfolio
-        port = db.execute("SELECT * FROM portfolio WHERE userid=:ids AND symbol=:symbol",ids=session["user_id"], symbol=results["symbol"])
+        port = Portfolio.query.filter_by(userid=session["user_id"], symbol=results["symbol"]).first()
         if not port:
-            db.execute("INSERT INTO portfolio (userid,stock,amount,symbol) VALUES (:ids, :name, :amount, :symbol)", ids=session["user_id"], name=results["name"],
-            amount=shares, symbol=results["symbol"])
+            new_port = Portfolio(userid=session["user_id"], stock=results["name"], amount=shares, symbol=results["symbol"])
+            db.session.add(new_port)
+            db.session.commit()
         else:
-            ntotal = port[0]["amount"]+shares
-            db.execute("UPDATE portfolio SET amount=:ntotal WHERE userid=:ids AND symbol=:symbol", ntotal=ntotal, ids=session["user_id"], symbol=results["symbol"])
+            ntotal = port.amount+shares
+            port.amount = ntotal
+            db.session.commit()
 
       # Redirect user to home page
         return redirect("/")
@@ -122,7 +164,7 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    history = db.execute("SELECT * FROM history WHERE userid=:ids",ids=session["user_id"])
+    history = History.query.filter_by(userid=session["user_id"]).all()
     j = len(history)
     return render_template("history.html", history=history, j=j)
 
@@ -147,14 +189,14 @@ def login():
 
         # Query database for username
         usern = request.form.get("username")
-        rows = db.execute("SELECT * FROM users WHERE username = ?", usern.lower())
+        rows = Users.query.filter_by(username = usern.lower()).first()
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if not check_password_hash(rows.hash, request.form.get("password")):
             return apology("invalid username and/or password", 400)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows.id
 
         # Redirect user to home page
         return redirect("/")
@@ -217,15 +259,17 @@ def register():
 
         # check if username exists
         username = request.form.get("username")
-        duplicate = db.execute("SELECT username FROM users WHERE username = :username", username=username.lower())
+        duplicate = Users.query.filter_by(username = username.lower()).first()
         if not duplicate:
             # add to database
             email = request.form.get("email")
-            # check50 has an issue with blank emails, so placeholder
+            #issue with blank emails, so placeholder
             if not email:
                 email = "empty"
             password = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
-            db.execute("INSERT INTO users (username, hash, email) VALUES (:username, :password, :email)", username=username.lower(), password=password, email=email)
+            new_user = Users(username=username.lower(), hash=password, email=email, cash=10000)
+            db.session.add(new_user)
+            db.session.commit()
             return redirect("/login")
         else:
             return apology("username already exists", 400)
@@ -251,35 +295,40 @@ def sell():
         elif shares <= 0:
             return apology("Please input a valid number of shares")
 
-        port = db.execute("SELECT * FROM portfolio WHERE userid=:ids AND symbol=:symbol", ids=session["user_id"], symbol=symbol)
-        user = db.execute("SELECT * FROM users WHERE id=:ids", ids=session["user_id"])
+        port = Portfolio.query.filter_by(userid=session["user_id"],symbol=symbol).first()
+        user = Users.query.filter_by(id=session["user_id"]).first()
         # if user does not have this stock
         if not port:
             return apology("You do not own this stock")
         # if user does not have enough
-        elif port[0]["amount"] < shares:
+        elif port.amount < shares:
             return apology("You do not own enough shares")
         else:
             results = lookup(symbol)
             cost = results["price"] * shares
-            balance = cost + user[0]["cash"]
-            db.execute("UPDATE users SET cash=:balance WHERE id=:ids", balance=balance, ids=session["user_id"])
-            db.execute("INSERT INTO history (userid, name, symbol, price, cost, amount, type, balance) VALUES (:ids, :name, :symbol, :price, :cost, :amount, :ttype, :balance)",
-                       ids=session["user_id"], name=results["name"], symbol=results["symbol"], price=results["price"], cost=cost, amount=shares, ttype="sell", balance=balance)
-
+            balance = cost + user.cash
+            user.cash=balance
+            db.session.commit()
+            history_new = History(userid=session["user_id"], name=results["name"], symbol=results["symbol"], 
+            price=results["price"], cost=cost, amount=shares, type="sell", balance=balance)
+            db.session.add(history_new)
+            db.session.commit()
+            
             #update portfolio
-            port = db.execute("SELECT * FROM portfolio WHERE userid=:ids AND symbol=:symbol", ids=session["user_id"], symbol=results["symbol"])
-            if port[0]["amount"] == shares:
-                db.execute("DELETE FROM portfolio WHERE userid=:ids AND symbol=:symbol", ids=session["user_id"],symbol=symbol)
+            port = Portfolio.query.filter_by(userid=session["user_id"],symbol=results["symbol"]).first()
+            if port.amount == shares:
+                db.session.delete(port)
+                db.session.commit()
+                
             else:
-                ntotal = port[0]["amount"]-shares
-                db.execute("UPDATE portfolio SET amount=:ntotal WHERE userid=:ids AND symbol=:symbol", ntotal=ntotal, ids=session["user_id"], symbol=results["symbol"])
-
+                ntotal = port.amount-shares
+                port.amount = ntotal
+                db.session.commit()
         return redirect("/")
 
     # get sell page with user portfolio
     else:
-        portfolio = db.execute("SELECT * FROM portfolio WHERE userid=:ids", ids=session["user_id"])
+        portfolio = Portfolio.query.filter_by(userid=session["user_id"]).all()
         j = len(portfolio)
         return render_template("sell.html", j=j, portfolio=portfolio)
 
@@ -290,8 +339,9 @@ def topup():
     # get topup page using Rave from flutterwave
     if request.method == "GET":
         key = os.environ.get("RAVE_KEY")
-        user = db.execute("SELECT * FROM users WHERE id=:ids", ids=session["user_id"])
-        return render_template("topup.html", user=user, key=key)
+        callback = os.environ.get("PAYMENT_CALLBACK")
+        user = Users.query.filter_by(id=session["user_id"]).first()
+        return render_template("topup.html", user=user, key=key, callback=callback)
 
     # process the payment response
     else:
@@ -300,9 +350,11 @@ def topup():
         if response["status"] == 'successful':
             # calculate new cash balance
             topamount = response["amount"]
-            cash = db.execute("SELECT cash FROM users WHERE id=:ids", ids=session["user_id"])
-            balance = float(topamount+cash[0]['cash'])
-            db.execute("UPDATE users SET cash=:balance WHERE id=:ids", balance=balance, ids=session["user_id"])
+            usern = Users.query.filter_by(id=session["user_id"]).first()
+            cash = usern.cash
+            balance = float(topamount+cash)
+            usern.cash = balance
+            db.session.commit()
             return ("200")
         else:
             return apology("Transaction failed")
